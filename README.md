0fc28cbf5e1fe8dffab42136d62f5a2bbd23d7cb
joel-medallia <109184597+joel-medallia@users.noreply.github.com>
2023-04-06 09:35:31 -0700

    docs(readme): Readme support and docs (#3)

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# sense360-ios-sdk
+<img src="https://user-images.githubusercontent.com/109184597/229904478-59d1d0ea-7079-4ce2-bc1b-40eaf7885759.png" width="450">
+
+## Medallia Sense360 SDK for iOS
+To get started with the Medallia Sense360 SDK for iOS, please follow our [online documentation](https://docs.medallia.com/?resourceId=sense360-ios-getting-started).
+
+## Questions or need help
+For any Medallia Sense360 SDK questions, please submit a support ticket [here](https://help.medallia.com/).
+
+## Other
---
+++ SenseQuinoaLib.xcframework/ios-arm64_x86_64-simulator/SenseQuinoaLib.framework/Headers/QuinoaIOS.h
@@ -0,0 +1,19 @@
+//
+//  QuinoaIOS.h
+//  QuinoaIOS
+//
+//  Created by dan bright on 5/29/15.
+//  Copyright (c) 2015 sense360. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for QuinoaIOS.
+FOUNDATION_EXPORT double QuinoaIOSVersionNumber;
+
+//! Project version string for QuinoaIOS.
+FOUNDATION_EXPORT const unsigned char QuinoaIOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <QuinoaIOS/PublicHeader.h>
+
+
