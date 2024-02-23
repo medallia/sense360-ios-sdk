eb47ffc0cd9d0227db8a0e795906fc8e3fe145f1
Continuous Deliver (DEN) <service-fowler@medallia.com>
2024-02-23 11:49:34 -0500

    publishing 9.9.9

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,8 +1,4 @@
-#### 4.6.3
+#### 5.0.0
 
-* [NEW] Added ability to integrate the Sense360 SDK via Swift Package Manager. For further details on this see our [online documentation](https://docs.medallia.com/en/?resourceId=sense360-ios-adding-spm).
-* [NEW] Added the new Sense360Testing API.
-* [UPDATE] Removed the visit data storage from the SDK that is older than 7 days.
-* [UPDATE] Updated location permission precision to add support for iOS 14+.
-* [UPDATE] Disabled the SDK outside of the US.
-* [UPDATE] Documentation released. Please, find it [here](https://docs.medallia.com/en/?resourceId=sense360-ios-getting-started).
+* [NEW] Some thing new
+* [UPDATE] Some thing updated
--- Package.swift
+++ Package.swift
@@ -1,20 +1,38 @@
 // swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
-
 import PackageDescription
 
 let package = Package(
     name: "medallia-sense360-ios-sdk",
     platforms: [
-        .iOS(.v11)
+        .iOS(.v13)
     ],
     products: [
         .library(
             name: "medallia-sense360-ios-sdk",
-            targets: ["SenseQuinoaLib"])
+            targets: ["SenseQuinoaLibWrapper"]
+        )
+    ],
+    dependencies: [
+        .package(name: "SwiftyJSON", url: "https://github.com/SwiftyJSON/SwiftyJSON.git", from: "5.0.1"),
+        .package(name: "SwiftJWT", url: "https://github.com/Kitura/Swift-JWT.git", from: "4.0.1"),
+        .package(name: "Reachability", url: "https://github.com/ashleymills/Reachability.swift.git", from: "5.0.0"),
+        .package(name: "DataCompression", url: "https://github.com/mw99/DataCompression.git", from: "3.8.0")
     ],
     targets: [
         .binaryTarget(
             name: "SenseQuinoaLib",
-            path: "SenseQuinoaLib.xcframework")
-    ])
+            path: "SenseQuinoaLib.xcframework"
+        ),
+        .target(name: "SenseQuinoaLibWrapper",
+                dependencies: [
+                    .target(name: "SenseQuinoaLib", condition: .when(platforms: .some([.iOS]))),
+                    "SwiftyJSON",
+                    "SwiftJWT",
+                    "Reachability",
+                    "DataCompression"
+                ],
+                path: "SenseQuinoaLibWrapper"
+        )
+    ]
+)
